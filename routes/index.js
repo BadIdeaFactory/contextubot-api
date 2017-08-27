4ec8047707f45640d402cb5caab823cf1e805017
Laurian Gridinoc <laurian@gridinoc.name>
2017-08-27 12:19:55 +0300

    poke travis

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+  res.json({ title: 'The Glorious Contextubot' });
 });
 
 module.exports = router;
