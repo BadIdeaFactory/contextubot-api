284618db1bb69260b1de5fc1592a36753468a156
Laurian Gridinoc <laurian@gridinoc.name>
2017-09-05 13:30:46 +0300

    url / mime -> s3

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ import debug from 'debug';
 import fs from 'fs';
 import { promisify } from 'util';
 
+import uuidv4 from 'uuid/v4';
+import mime from 'mime-types';
 import Promise from 'promise';
 import AWS from 'aws-sdk';
 import tmp from 'tmp';
+import request from 'requisition';
 
 import express from 'express';
 import logger from 'morgan';
@@ -15,8 +18,7 @@ import self from '../package';
 
 const log = debug('contextubot:api');
 
-// const writeFile = promisify(fs.writeFile);
-// const readFile = promisify(fs.readFile);
+const readFile = promisify(fs.readFile);
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ID,
@@ -25,23 +27,14 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
-// const getObject = (params) => {
-//   return new Promise((fulfill, reject) => {
-//     s3.getObject(params, (err, res) => {
-//       if (err) reject(err);
-//       else fulfill(res);
-//     });
-//   });
-// };
-//
-// const putObject = (params) => {
-//   return new Promise((fulfill, reject) => {
-//     s3.putObject(params, (err, res) => {
-//       if (err) reject(err);
-//       else fulfill(res);
-//     });
-//   });
-// };
+const putObject = (params) => {
+  return new Promise((fulfill, reject) => {
+    s3.putObject(params, (err, res) => {
+      if (err) reject(err);
+      else fulfill(res);
+    });
+  });
+};
 
 const app = express();
 tmp.setGracefulCleanup();
@@ -59,10 +52,44 @@ app.get('/', (req, res) => {
   ]);
 });
 
-app.post('/', (req, res) => {
+app.post('/fingerprint', async (req, res) => {
   log(req.body);
-  res.send({});
+  const { url } = req.body;
+  res.send({ data: await processURL(url) });
 });
+
+const processURL = async (url) => {
+  const id = uuidv4();
+  const dir = tmp.dirSync();
+  log(id, dir.name);
+
+  // download
+  const response = await request(url);
+  log(response.headers);
+
+  let contentType = 'application/octet-stream';
+  if (response.headers['content-type']) contentType = response.headers['content-type'];
+
+  const extension = mime.extension(contentType);
+  await response.saveTo(`${dir.name}/${id}.${extension}`)
+
+  // upload (s3)
+  const data = await readFile(`${dir.name}/${id}.${extension}`);
+  const result = await putObject({
+    Body: data,
+    ACL: 'public-read',
+    ContentType: contentType,
+    Bucket: 'ingest.contextubot.net', // TODO -> ENV
+    Key: `test/${id}/${id}.${extension}`
+  });
+  log(result);
+
+  // dir.removeCallback(); // FIXME ENOTEMPTY: directory not empty, rmdir
+
+  // compute link
+  log(`https://s3.amazonaws.com/fingerprints.contextubot.net/test/${id}/${id}.afpt`);
+  return `https://s3.amazonaws.com/fingerprints.contextubot.net/test/${id}/${id}.afpt`;
+}
 
 
 // Catch 404 and forward to error handler
